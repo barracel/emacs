f2ece5d07009c5c1a9503534a7dd8343cdbda5a3
barracel <fernandez.barracel@gmail.com>
2013-08-27 23:55:04 +0200

    Removed lucid compatibility

--- custom/my-ui.el
+++ custom/my-ui.el
@@ -11,7 +11,6 @@
       (set-face-background 'isearch zenburn-fg)
       (set-face-foreground 'isearch zenburn-bg)
       (set-face-background 'lazy-highlight zenburn-fg-1)
-      (set-face-background 'highlight-symbol-face zenburn-fg-1)
 
       (set-face-attribute  'mode-line
                            nil
@@ -23,12 +22,4 @@
                            :box '(:line-width 1 :style released-button))
 ))
 
-
-
-(desktop-save-mode 1)
 (scroll-bar-mode -1)
-
-; zenburn is not handling correctly colors in my lucid's emacs
-( if (string-equal (shell-command-to-string "lsb_release -cs") "lucid\n")
-    (setq ansi-term-color-vector [unspecified "#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
-)
